72daaf32afbdbc34c496a5dfa38fc772a634ac31
Walter Geisler <soren.endon@gmail.com>
2017-09-15 22:49:10 -0500

    Implemented more UI

---
+++ Assets/_TTicc-TTacc-TToee/_src/_Codari/_TTT/_UI/Button Actions/QuickStartUIButtonAction.cs
@@ -0,0 +1,17 @@
+﻿using Zenject;
+
+using Codari.TTT.Network;
+
+namespace Codari.TTT.UI
+{
+    internal sealed class QuickStartUIButtonAction : UIButtonAction
+    {
+        [Inject]
+        private TTTNetworkManager tttNetworkManager;
+
+        protected override void Action()
+        {
+            //tttNetworkManager.StartMatchMaker();
+        }
+    }
+}
